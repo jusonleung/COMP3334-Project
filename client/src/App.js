b5996c5e56e7ed5e12a180332bc5cc6738ffc838
jusonleung <jusonleung@gmail.com>
2023-04-06 03:32:35 +0800

    Create Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+/* import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import EmailVerify from "./components/EmailVerify"; */
+
 function App() {
-  return (
-      <div>
-          <p>Hello World!</p>
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                {/* <Route path='/register' element={<Signup />} />
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='email/verify' element={<EmailVerify />} /> */}
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Button, Form, Input } from 'antd'
+import { useNavigate } from 'react-router-dom'
+
+const App: React.FC = () => {
+  const navigate = useNavigate()
+  const gotoSignUpPage = () => navigate('/register')
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
+  return (
+    <div className='login__container'>
+      <h2>Login </h2>
+
+      <Form
+        name='basic'
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete='off'
+      >
+        <Form.Item
+          label='Username'
+          name='username'
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Password'
+          name='password'
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type='primary' htmlType='submit'>
+            Sign in
+          </Button>
+        </Form.Item>
+      </Form>
+      <p>
+        Don't have an account?{' '}
+        <span className='link' onClick={gotoSignUpPage}>
+          Sign up
+        </span>
+      </p>
+    </div>
+  )
+}
+
+export default App
