2a4234c80eb7078093fbd312759f907e1583074a
jusonleung <jusonleung@gmail.com>
2023-04-06 21:07:43 +0800

    Added key and cert for https

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
 
+const options = {
+    key: fs.readFileSync("key"),
+    cert: fs.readFileSync("cert")
+};
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ message: "Hello world" });
 });
 
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
